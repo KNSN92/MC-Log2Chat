69b69ca4817f577cdfaea40ec58f8c53fa11c98d
KNSN92 <kekenshin92@gmail.com>
2025-08-25 11:26:16 +0900

    カテゴリ名がPlayersになっていたのを修正

--- src/components/chat-type-selector.tsx
+++ src/components/chat-type-selector.tsx
@@ -30,7 +30,11 @@ export default function ChatTypeSelector({
   const [open, setOpen] = useState(false);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const selectedChatTypeList = selectedChatTypes ? Object.entries(selectedChatTypes).filter(([_, selected]) => selected).map(([chatType]) => chatType) : [];
+  const selectedChatTypeList = selectedChatTypes
+    ? Object.entries(selectedChatTypes)
+        .filter(([_, selected]) => selected)
+        .map(([chatType]) => chatType)
+    : [];
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -43,7 +47,9 @@ export default function ChatTypeSelector({
           className="w-[20vw] min-w-fit max-w-[200px] justify-between"
         >
           {selectedChatTypeList.length > 0
-            ? (chatTypeFilteringMode === "whitelist" ? "" : "!") + selectedChatTypeList[0] + (selectedChatTypeList.length > 1 ? ", ..." : "")
+            ? (chatTypeFilteringMode === "whitelist" ? "" : "!") +
+              selectedChatTypeList[0] +
+              (selectedChatTypeList.length > 1 ? ", ..." : "")
             : "Select chat type..."}
           <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -51,46 +57,61 @@ export default function ChatTypeSelector({
       <PopoverContent className="w-[200px] p-0">
         <Command>
           <CommandGroup heading="Settings">
-            <CommandItem className="pl-6" onSelect={() => togglePlayerFilteringMode()}>
+            <CommandItem
+              className="pl-6"
+              onSelect={() => togglePlayerFilteringMode()}
+            >
               <div className="mr-2 size4" />
-              {chatTypeFilteringMode === "whitelist" ? "Whitelist" : "Blacklist"}
+              {chatTypeFilteringMode === "whitelist"
+                ? "Whitelist"
+                : "Blacklist"}
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
           <CommandList>
             <CommandEmpty>No player found.</CommandEmpty>
-            <CommandGroup heading="Players">
-              {(Object.keys(ChatTypeFilterDefault) as ChatType[]).sort().map((chatType, i) => (
-                <CommandItem
-                  key={i}
-                  value={chatType}
-                  onSelect={(currentValue) => {
-                    const newSelectedChatTypes: ChatTypeFilter = selectedChatTypes ? {
-                      ...selectedChatTypes,
-                    } : {...ChatTypeFilterDefault};
-                    if(Object.keys(newSelectedChatTypes).includes(currentValue)) {
-                      const chatType = currentValue as ChatType;
-                      newSelectedChatTypes[chatType] = !newSelectedChatTypes[chatType];
-                      if(Object.values(newSelectedChatTypes).some((v) => v)) {
-                        setSelectedChatTypes(newSelectedChatTypes);
-                      }else {
-                        setSelectedChatTypes(null);
+            <CommandGroup heading="Chat Types">
+              {(Object.keys(ChatTypeFilterDefault) as ChatType[])
+                .sort()
+                .map((chatType, i) => (
+                  <CommandItem
+                    key={i}
+                    value={chatType}
+                    onSelect={(currentValue) => {
+                      const newSelectedChatTypes: ChatTypeFilter =
+                        selectedChatTypes
+                          ? {
+                              ...selectedChatTypes,
+                            }
+                          : { ...ChatTypeFilterDefault };
+                      if (
+                        Object.keys(newSelectedChatTypes).includes(currentValue)
+                      ) {
+                        const chatType = currentValue as ChatType;
+                        newSelectedChatTypes[chatType] =
+                          !newSelectedChatTypes[chatType];
+                        if (
+                          Object.values(newSelectedChatTypes).some((v) => v)
+                        ) {
+                          setSelectedChatTypes(newSelectedChatTypes);
+                        } else {
+                          setSelectedChatTypes(null);
+                        }
                       }
-                    }
-                    setOpen(false);
-                  }}
-                >
-                  <CheckIcon
-                    className={cn(
-                      "mr-2 size4",
-                      selectedChatTypes && selectedChatTypes[chatType]
-                        ? "opacity-100"
-                        : "opacity-0"
-                    )}
-                  />
-                  {chatType}
-                </CommandItem>
-              ))}
+                      setOpen(false);
+                    }}
+                  >
+                    <CheckIcon
+                      className={cn(
+                        "mr-2 size4",
+                        selectedChatTypes && selectedChatTypes[chatType]
+                          ? "opacity-100"
+                          : "opacity-0"
+                      )}
+                    />
+                    {chatType}
+                  </CommandItem>
+                ))}
             </CommandGroup>
           </CommandList>
         </Command>
